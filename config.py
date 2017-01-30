e25616dbabe0984b321204552faf504a20facf90
linanwx <linanwx@gmail.com>
2017-01-31 00:23:18 +0800

    add md design

--- config.py
+++ config.py
@@ -7,9 +7,10 @@ class Config:
     SSL_DISABLE = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_RECORD_QUERIES = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_PORT = 465                                             # 使用SSL加密方式，端口为465
     MAIL_USE_SSL = True                                         # 若你使用的邮箱服务是其他端口，或者其他加密方式，请在这里修改
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')          
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_ASCII_ATTACHMENTS = True
@@ -17,6 +18,7 @@ class Config:
     FLASKY_MAIL_SENDER = os.environ.get('FLASKY_MAIL_SENDER')
     POSTS_PER_PAGE = os.environ.get('POSTS_PER_PAGE')
     FLASKY_SLOW_DB_QUERY_TIME = 0.5
+    BOOTSTRAP_USE_CDN = True
 
     @staticmethod
     def init_app(app):
